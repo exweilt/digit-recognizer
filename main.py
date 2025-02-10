529aef3ad9a0f89fb36d974febb4395e98ff3eb6
exweilt <liapkalo@tcd.ie>
2025-02-10 15:10:12 +0000

    add Layer class

--- main.py
+++ main.py
@@ -12,18 +12,14 @@ WIDTH=28*PIXEL_SIZE
 HEIGHT=28*PIXEL_SIZE
 
 if __name__ == "__main__":
-    # Initialize Pygame
     pygame.init()
 
-    # Set up the display window
     window_size = (WIDTH*2, HEIGHT*2)
     window = pygame.display.set_mode(window_size)
     pygame.display.set_caption('Neural Network')
 
-    # Fill the window with a white background
     window.fill((200, 200, 200))
 
-    # Update the display
     pygame.display.update()
 
     images = utils.parse_mnist(5)
@@ -32,6 +28,7 @@ if __name__ == "__main__":
     # # set_virtual_pixel(27*PIXEL_SIZE, 27*PIXEL_SIZE, 0)
     # set_virtual_pixel(27, 27, 0)
 
+    # Draw mnist digit
     for row_idx, row in enumerate(images[4]):
         for col_idx, grayscale in enumerate(row):
             # window.set_at((col_idx, row_idx), (grayscale, grayscale, grayscale))
@@ -40,10 +37,10 @@ if __name__ == "__main__":
 
     
         
-
     # nn = neunet.NeuralNetwork.init_layered_network([28*28, 256, 128, 10])
+    nn = neunet.NeuralNetwork.init_layered_network([3, 6, 2])
 
-    # draw_neural_network(nn)
+    utils.draw_neural_network(nn)
 
     pygame.display.update()
 
--- neunet.py
+++ neunet.py
@@ -1,3 +1,6 @@
+import numpy as np
+from typing import *
+
 class Link:
     def __init__(self) -> None:
         self.origin: Neuron
@@ -9,6 +12,7 @@ class Neuron:
         self.bias: float = 0
         self.links: list[Link] = []
         self.visual_pos: tuple[float, float] = (0, 0)
+        self.visual_layer: int = -1
 
     def register_link(self, destination: 'Neuron'):
         pass
@@ -19,22 +23,37 @@ class Neuron:
 
 class NeuralNetwork:
     def __init__(self) -> None:
-        pass
+        # input_neurons: List[Neuron] = []
+        self.neuron_layers: List[Neuron] = []
 
     @staticmethod
     def init_layered_network(layers_sizes: list[int]) -> 'NeuralNetwork':
+        """
+        The first layer does not contain neurons
+        """
         assert len(layers_sizes) >= 2
 
         nn = NeuralNetwork()
 
-        for node_idx in range(layers_sizes[0]):
-            nn.register_input_neuron()
-        for node_idx in range(layers_sizes[-1]):
-            nn.register_output_neuron()
+        nn.neuron_layers = [[]] * len(layers_sizes)
+
+        for layer_idx in range(len(layers_sizes)):
+            for neuron_idx in range(layers_sizes[layer_idx]):
+                new_neuron = Neuron()
+
+                nn.neuron_layers[layer_idx].append(new_neuron)
+
+                new_neuron.visual_pos = (10 * neuron_idx, 50 * layer_idx)
+                new_neuron.visual_layer = layer_idx
 
 
-        
-        
+
+            
+
+        # for node_idx in range(layers_sizes[0]):
+        #     nn.register_input_neuron()
+        # for node_idx in range(layers_sizes[-1]):
+        #     nn.register_output_neuron()
 
     @staticmethod
     def load_network_from_json() -> 'NeuralNetwork':
@@ -48,9 +67,17 @@ class NeuralNetwork:
     
     # if index = -1, adds it to the end
     def register_input_neuron(self, neuron: Neuron, index: int = -1) -> None:
-        pass
+        self.input_neurons.append(neuron)
 
     # if index = -1, adds it to the end
     def register_output_neuron(self, neuron: Neuron, index: int = -1) -> None:
         pass
 
+class DenseLayer:
+    def __init__(self, neuron_num: int):
+        self.w = np.random.randn(neuron_num)
+        self.b = 0
+    
+    # Sigmoid
+    def activate(x, k=1.0):
+        return 1 / (1 + np.exp(-k*x))
